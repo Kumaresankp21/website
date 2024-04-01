54190b07c72739dc381edd2bfed3ca2ae047eac7
Kumaresankp21 <142023883+Kumaresankp21@users.noreply.github.com>
2024-04-01 19:04:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const faders_elements = document.querySelectorAll('.fade-in-ani');
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -200px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('appear-ani');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+faders_elements.forEach(fader => appearOnScroll.observe(fader))
